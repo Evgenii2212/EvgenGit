25ca59a7403e448519a00f8067b65ebb3693dc7a
Evgenii2212 <166916019+Evgenii2212@users.noreply.github.com>
2024-04-27 14:02:16 +0700

    Update massiv1.py

--- massiv/massiv1.py
+++ massiv/massiv1.py
@@ -1,12 +1,26 @@
-def find_separation_binary(arr):
-  left, right = 0, len(arr) - 1
-  while left < right:
-    mid = (left + right) // 2
-    if arr[mid] == 0 and arr[mid + 1] == 1:
-      return mid
-    elif arr[mid] == 0:
-      left = mid + 1
-    else:
-      right = mid
-  return -1
+def find_division_index(arr):
+    left = 0
+    right = len(arr) - 1
+    result = -1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        if arr[mid] == 1 and arr[mid-1] == 0:
+            result = mid
+            break
+        elif arr[mid] == 0:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return result
 
+arr = list(map(int, input("Введите элементы массива через пробел: ").split()))
+
+division_index = find_division_index(arr)
+
+if division_index != -1:
+    print(f"Место разделения: Индекс {division_index}")
+else:
+    print("Разделение не найдено.")
